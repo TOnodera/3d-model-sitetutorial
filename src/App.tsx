25d25d85637c1c48cfd6c2cef02e36c4068a8914
t.onodera <t.onodera.1.2.1.5@gmail.com>
2023-01-14 21:50:46 +0900

    柴犬表示

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import './App.css'
 import * as THREE from 'three';
 import { useEffect } from 'react';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
 
 function App() {
+
+  let model: THREE.Group;
 
   useEffect(()=>{
     const canvas: HTMLCanvasElement = document.querySelector("#canvas") as HTMLCanvasElement;
@@ -14,20 +17,23 @@ function App() {
     const scene = new THREE.Scene();
     // camera 
     const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height, 0.1, 1000);
-    camera.position.set(1,1,3);
+    camera.position.set(0.5,-0.5,2);
     // rendererk
     const renderer = new THREE.WebGLRenderer({
       canvas,
-      antialias: true
+      antialias: true,
+      alpha: true
     });
 
     renderer.setSize(sizes.width, sizes.height);
     renderer.setPixelRatio(window.devicePixelRatio);
 
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial( {color: 0x00ff00} );
-    const cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    const gltfLoader = new GLTFLoader();
+    gltfLoader.load("./models/shiba.gltf", (gltf)=>{
+      model = gltf.scene;
+      model.rotateY(-0.5);
+      scene.add(model);
+    });    
 
     // アニメーション
     const tick = ()=>{
@@ -42,7 +48,10 @@ function App() {
   return (
     <>
     <canvas id="canvas"></canvas>
-    <div className="mainContent"></div>
+    <div className="mainContent">
+      <h2>T Code</h2>
+      <p>web developer</p>
+    </div>
     </>
   )
 }
